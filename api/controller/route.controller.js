17119d783f4ca873983483e3d5d362a85a6d13d2
Nguyen Tran Hoang An <hoangan726@gmail.com>
2021-09-22 14:55:55 +0700

    Push all processing at the present

---
+++ api/controller/route.controller.js
@@ -0,0 +1,41 @@
+const service = require('../services/service')
+
+const cal_route = (req,res)=> {
+    service.cal_route((data)=> {
+      res.json({data : data})
+    }, 'scooter')
+}
+
+const cal_route_via = (req,res)=> {
+    service.cal_route_via((data) => {
+      res.json({data : data})
+    },'scooter')
+}
+
+const cal_route_mulvia = (req,res) => {
+    service.cal_route_mulvia((data)=> {
+      res.json({data : data})
+    }, 'scooter')
+}
+
+const routeInstruction = (req,res)=> {
+    service.routeInstruction((data)=> {
+      res.json({Instruction : data})
+         .status(200)
+    })
+}
+
+const routeTurnbyturn = (req,res)=> {
+    service.routeTurnbyturn((data)=> {
+      res.json({data : data})
+         .status(200)
+    })
+}
+
+module.exports = {
+    cal_route,
+    cal_route_via,
+    cal_route_mulvia,
+    routeInstruction,
+    routeTurnbyturn
+}
---
+++ api/services/service.js
@@ -0,0 +1,94 @@
+const needle = require('needle')
+const nhatui = '10.82397,106.68567';
+const emartSupermarket = '10.82291,106.69284'
+const papaChicken_via = '10.82543,106.69063' 
+const snobCoffee_via = '10.82469,106.69169'
+
+
+const cal_route = async (result,transportMode) => {
+    if(!transportMode) {
+        transportMode = 'car';
+    }
+    const url = `https://router.hereapi.com/v8/routes?transportMode=${transportMode}&return=polyline,summary&origin=${nhatui}&destination=${emartSupermarket}&apikey=${process.env.REST_API_KEY}`;
+    needle('get', url)
+        .then(function (resp) {
+            result(resp.body);
+        })
+        .catch(function (err) {
+            console.log(err)
+        });
+}
+
+
+const cal_route_via = (result,transportMode,via) => {
+    if(!transportMode) {
+        transportMode = 'car';
+    }
+    if(!via) {
+        via = papaChicken_via;
+    }
+    const url = `https://router.hereapi.com/v8/routes?transportMode=${transportMode}&return=polyline,summary,passthrough&origin=${nhatui}&destination=${emartSupermarket}&via=${via}&apikey=${process.env.REST_API_KEY}`;
+    needle('get', url)
+        .then(function (resp) {
+            result(resp.body);
+        })
+        .catch(function (err) {
+            console.log(err)
+        });
+}
+
+const cal_route_mulvia = (result,transportMode) => {
+    if(!transportMode) {
+        transportMode = 'car';
+    }
+    const url = `https://router.hereapi.com/v8/routes?transportMode=${transportMode}&return=polyline,summary,passthrough&origin=${nhatui}&destination=${emartSupermarket}&via=${papaChicken_via}&via=${snobCoffee_via}&apikey=${process.env.REST_API_KEY}`
+    needle('get', url)
+    .then(function (resp) {
+        result(resp.body);
+    })
+    .catch(function (err) {
+        console.log(err)
+    });
+}
+
+const routeInstruction = (result) => {
+    const transportMode = 'scooter'
+    const url = `https://router.hereapi.com/v8/routes?transportMode=${transportMode}&return=polyline,actions,instructions&origin=${nhatui}&destination=${emartSupermarket}&via=${snobCoffee_via}&lang=vi&apikey=${process.env.REST_API_KEY}`
+    needle('get', url)
+    .then(function (resp) {
+        var sections =  resp.body.routes[0].sections
+        var instruction = {}
+        for(let i=0; i<sections.length; i++) {
+            var actions = sections[i].actions;
+            instruction[i] = {}
+            for(let j=0; j<actions.length; j++) {
+                instruction[i][j] = actions[j].instruction;
+                if(actions[j].action === 'arrive') {break;}
+            }
+        } 
+        result(instruction);
+    })
+    .catch(function (err) {
+        console.log(err)
+    });
+}
+
+const routeTurnbyturn = (result) => {
+    const transportMode = 'scooter'
+    const url = `https://router.hereapi.com/v8/routes?transportMode=${transportMode}&return=polyline,turnbyturnactions&origin=${nhatui}&destination=${emartSupermarket}&apikey=${process.env.REST_API_KEY}`
+    needle('get', url)
+    .then(function (resp) {
+        result(resp.body);
+    })
+    .catch(function (err) {
+        console.log(err)
+    });
+}
+
+module.exports = {
+    cal_route,
+    cal_route_via,
+    cal_route_mulvia,
+    routeInstruction,
+    routeTurnbyturn
+}
